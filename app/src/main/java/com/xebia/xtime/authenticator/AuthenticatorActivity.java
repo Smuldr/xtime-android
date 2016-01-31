a3f1ac587ef3e64b9953eafeec8926dab363c226
Steven Mulder <steven.mulder@gmail.com>
2016-01-31 17:57:25 +0100

    Fix AppCompat theme for AuthenticatorActivity

--- app/src/main/java/com/xebia/xtime/authenticator/AuthenticatorActivity.java
+++ app/src/main/java/com/xebia/xtime/authenticator/AuthenticatorActivity.java
@@ -1,13 +1,14 @@
 package com.xebia.xtime.authenticator;
 
 import android.accounts.Account;
-import android.accounts.AccountAuthenticatorActivity;
+import android.accounts.AccountAuthenticatorResponse;
 import android.accounts.AccountManager;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -27,7 +28,7 @@ import timber.log.Timber;
 /**
  * Activity which displays a login screen to the user
  */
-public class AuthenticatorActivity extends AccountAuthenticatorActivity {
+public class AuthenticatorActivity extends AppCompatActivity {
 
     public static final String KEY_ADD_NEW_ACCOUNT = "com.xebia.xtime.extra.ADD_NEW_ACCOUNT";
     /**
@@ -44,9 +45,36 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
     private View mLoginStatusView;
     private TextView mLoginStatusMessageView;
 
+    // used to call back to the AccountManager
+    private AccountAuthenticatorResponse mAccountAuthenticatorResponse = null;
+    private Bundle mResultBundle = null;
+
+    /**
+     * Set the result that is to be sent as the result of the request that caused this
+     * Activity to be launched. If result is null or this method is never called then
+     * the request will be canceled.
+     * @param result this is returned as the result of the AbstractAccountAuthenticator request
+     */
+    private void setAccountAuthenticatorResult(Bundle result) {
+        mResultBundle = result;
+    }
+
+    /**
+     * Retreives the AccountAuthenticatorResponse from either the intent of the icicle, if the
+     * icicle is non-zero.
+     *
+     * @param icicle the save instance data of this Activity, may be null
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        mAccountAuthenticatorResponse =
+                getIntent().getParcelableExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE);
+
+        if (mAccountAuthenticatorResponse != null) {
+            mAccountAuthenticatorResponse.onRequestContinued();
+        }
 
         setContentView(R.layout.activity_login);
 
@@ -80,6 +108,24 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
                 attemptLogin();
             }
         });
+    }
+
+    /**
+     * Sends the result or a Constants.ERROR_CODE_CANCELED error if a result isn't present.
+     */
+    @Override
+    public void finish() {
+        if (mAccountAuthenticatorResponse != null) {
+            // send the result bundle back if set, otherwise send an error.
+            if (mResultBundle != null) {
+                mAccountAuthenticatorResponse.onResult(mResultBundle);
+            } else {
+                mAccountAuthenticatorResponse.onError(AccountManager.ERROR_CODE_CANCELED,
+                        "canceled");
+            }
+            mAccountAuthenticatorResponse = null;
+        }
+        super.finish();
     }
 
     /**
@@ -156,10 +202,10 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
                 });
     }
 
-    private void finishLogin(Cookie cookie) {
+    private void onLoginSuccess(final Cookie cookie) {
 
         // add/update account in account manager
-        AccountManager accountManager = AccountManager.get(this);
+        final AccountManager accountManager = AccountManager.get(this);
         final Account account = new Account(mUsername, Authenticator.ACCOUNT_TYPE);
         if (getIntent().getBooleanExtra(KEY_ADD_NEW_ACCOUNT, false)) {
             // Creating the account on the device and setting the auth token we got
@@ -225,7 +271,7 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
                 mPasswordView.requestFocus();
 
             } else {
-                finishLogin(cookie);
+                onLoginSuccess(cookie);
             }
         }
 
