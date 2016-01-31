b5c03cb6df307c5917da9601dd4faca33eb45825
Steven Mulder <steven.mulder@gmail.com>
2016-01-31 13:13:38 +0100

    Created LauncherActivity to check for XTime account
    Adds a new account using Android's AccountManager,
    or starts the main OverviewActivity.

---
+++ app/src/main/java/com/xebia/xtime/LauncherActivity.java
@@ -0,0 +1,74 @@
+package com.xebia.xtime;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.xebia.xtime.authenticator.Authenticator;
+
+import timber.log.Timber;
+
+/**
+ * Launcher activity
+ *
+ * Checks if an XTime account has been set up on the phone and starts the
+ * {@link OverviewActivity} if it has.  If there is not XTime account present, the
+ * {@link android.accounts.AccountManager} is invoked to add one.
+ */
+public class LauncherActivity extends Activity {
+
+    private AccountManager accountManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        accountManager = AccountManager.get(this);
+        if (hasXTimeAccount()) {
+            startOverviewActivity();
+        } else {
+            addAccount();
+        }
+    }
+
+    private boolean hasXTimeAccount() {
+        Account[] accounts = accountManager.getAccountsByType(Authenticator.ACCOUNT_TYPE);
+        return accounts.length > 0;
+    }
+
+    private void addAccount() {
+        Timber.d("Add account");
+        accountManager.addAccount(Authenticator.ACCOUNT_TYPE, Authenticator.AUTH_TYPE, null,
+                null, this, new XTimeAccountCallback(this), null);
+    }
+
+    private void startOverviewActivity() {
+        Timber.d("Start OverviewActivity");
+        startActivity(new Intent(this, OverviewActivity.class));
+        finish();
+    }
+
+    private static class XTimeAccountCallback implements AccountManagerCallback<Bundle> {
+
+        private final LauncherActivity activity;
+
+        XTimeAccountCallback(final LauncherActivity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public void run(AccountManagerFuture<Bundle> future) {
+            Timber.d("AccountManagerCallback called");
+            if (future.isDone()) {
+                activity.startOverviewActivity();
+            } else {
+                Timber.w("Something is very wrong with the AccountManager");
+                activity.finish();
+            }
+        }
+    }
+}
--- app/src/main/java/com/xebia/xtime/OverviewActivity.java
+++ app/src/main/java/com/xebia/xtime/OverviewActivity.java
@@ -1,11 +1,5 @@
 package com.xebia.xtime;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.accounts.AccountManagerCallback;
-import android.accounts.AccountManagerFuture;
-import android.accounts.AuthenticatorException;
-import android.accounts.OperationCanceledException;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,58 +10,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SimpleAdapter;
 
-import com.xebia.xtime.authenticator.Authenticator;
 import com.xebia.xtime.dayoverview.DayOverviewActivity;
 import com.xebia.xtime.monthoverview.MonthPagerFragment;
 import com.xebia.xtime.shared.model.DayOverview;
 import com.xebia.xtime.weekoverview.DailyHoursListFragment;
 import com.xebia.xtime.weekoverview.WeekPagerFragment;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import timber.log.Timber;
-
 public class OverviewActivity extends AppCompatActivity implements DailyHoursListFragment.Listener {
-
-    private AccountManager mAccountManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mAccountManager = AccountManager.get(this);
-        if (hasXTimeAccount()) {
-            setListNavigation();
-        } else {
-            Timber.d("Add account");
-            AccountManagerCallback<Bundle> callback = new AccountManagerCallback<Bundle>() {
-                @Override
-                public void run(AccountManagerFuture<Bundle> future) {
-                    try {
-                        Timber.d("AccountManagerCallback called");
-                        if (future.isDone()) {
-                            Bundle result = future.getResult();
-                            String authToken = result.getString(AccountManager.KEY_AUTHTOKEN);
-                            Timber.i("Auth token: '%s'", authToken);
-                            setListNavigation();
-                        }
-                    } catch (OperationCanceledException | IOException | AuthenticatorException e) {
-                        Timber.e(e, "Failed to get auth token");
-                    }
-                }
-            };
-            mAccountManager.addAccount(Authenticator.ACCOUNT_TYPE, Authenticator.AUTH_TYPE, null,
-                    null, this, callback, null);
-        }
-    }
-
-    private boolean hasXTimeAccount() {
-        Account[] accounts = mAccountManager.getAccountsByType(Authenticator.ACCOUNT_TYPE);
-        return accounts.length > 0;
+        setListNavigation();
     }
 
     private void setListNavigation() {
